661cc1ddb3a9a546e76451a2ccf0c67918c68e9e
fedurca <fedurca@gmail.com>
2023-04-01 07:30:13 +0200

    Update curl.py

--- curl.py
+++ curl.py
@@ -1,5 +1,9 @@
 import requests
 from requests.structures import CaseInsensitiveDict
+#from urllib2 import Request, urlopen
+import json
+import pandas as pd  
+
 
 url = "https://einfo.ceproas.cz/cepro_portal_ws/rest/common/prox/mobileData"
 
@@ -12,9 +16,13 @@ headers["content-type"] = "application/json; charset=UTF-8"
 headers["host"] = "einfo.ceproas.cz"
 headers["user-agent"] = "okhttp/4.9.0"
 
-data = "{  "search": {    "exclude_cs_ceny": false,   "exclude_cs_kvalita": false  },  "version": 5}"
+data = '{  "search": {    "exclude_cs_ceny": false,   "exclude_cs_kvalita": false  },  "version": 5}'
 
 
 resp = requests.post(url, headers=headers, data=data)
 
+json_object = json.dumps(resp.text)
+
+
+print(json_object)
 print(resp.status_code)
